14e1ba820a0f03ad5c37b4d71cc27b5c5f873452
Bohdan Vanchuhov <bohdan.vanchuhov@globallogic.com>
2018-08-04 15:29:50 +0300

    lessons 5-6

---
+++ src/IfElsePractice.java
@@ -0,0 +1,20 @@
+/**
+ * @author bvanchuhov
+ */
+public class IfElsePractice {
+
+    public static void main(String[] args) {
+        int n = 0;
+
+        if (n < 20) {
+            if (n > 0) {
+                System.out.println("0");
+            }
+            System.out.println("1");
+        } else {
+            System.out.println("2");
+        }
+
+        System.out.println("3");
+    }
+}
